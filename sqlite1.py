42c7a8a43449d886bbd9dcc075479dee4aece8a4
Feng <liwind93@qq.com>
2017-02-18 20:55:56 +0800

    complete python sqlite3

--- sqlite1.py
+++ sqlite1.py
@@ -5,3 +5,12 @@
 # @Date     : 2017/2/17
 
 import sqlite3
+
+conn = sqlite3.connect('test.db')
+curs = conn.cursor()
+sql = 'select * from user'
+curs.execute(sql)
+values = curs.fetchall()
+print values
+curs.close()
+conn.close()
